44adf264f5f134079a2de6aac5f56a7ea228414f
Czinege Lajos <90503068+czlaliii@users.noreply.github.com>
2021-11-15 21:01:32 +0100

    Add files via upload

---
+++ Ch8_drill/use.cpp
@@ -0,0 +1,16 @@
+#include "my.h"
+#include <iostream>
+
+int foo;
+
+int main()
+{
+	foo=7;
+	print_foo();
+	print(99);
+	char cc;
+	cout << "Enter any key to exit" << endl;
+	cin >> cc;
+	
+	return 0;
+}
